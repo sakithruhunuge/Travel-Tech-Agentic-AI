23fd3810da2b16d3ed872a5ee6c11fd5a49a6bbf
sakithruhunuge <sakithsandeev@gmail.com>
2026-09-05 09:37:24 +0530

    chore: initialize Next.js 14 project with TypeScript, ESLint, and PostCSS configuration

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default withNextIntl(nextConfig);
